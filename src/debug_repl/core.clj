f3ade84275656f6f2feaa8561950f40cd4f84dce
Gary Fredericks <fredericksgary@gmail.com>
2014-03-30 09:08:49 -0500

    OMG I think it works

--- src/debug_repl/core.clj
+++ src/debug_repl/core.clj
@@ -4,6 +4,11 @@
             [clojure.tools.nrepl.misc :refer [response-for]]
             [clojure.tools.nrepl.transport :as transport]))
 
+(defn stdout-println
+  "For debugging while developing debug-repl."
+  [& obs]
+  (.println System/out (apply print-str obs)))
+
 (defmacro locals
   []
   (into {}
@@ -42,8 +47,12 @@
 
 (defn break
   [locals ns]
-  (let [{:keys [transport], session-id ::session-id} *msg*
-        p (promise)
+  (let [{:keys [transport],
+         session-id ::orig-session-id
+         nest-session-fn ::nest-session}
+        *msg*
+
+        unbreak-p (promise)
         eval-requests (atom clojure.lang.PersistentQueue/EMPTY)
         pop-eval-request (fn []
                            (when-not (empty? @eval-requests)
@@ -55,13 +64,14 @@
                                  (meta)
                                  :popped)))]
     ;; TODO: nesting
-    (assert (not (contains? @info session-id)))
-    (swap! info assoc session-id
-           {:promise p
-            :eval    (fn [code]
-                       (let [result-p (promise)]
-                         (swap! eval-requests conj [code result-p])
-                         (uncatch @result-p)))})
+    (swap! info update-in [session-id] conj
+           {:unbreak           unbreak-p
+            :nested-session-id (nest-session-fn)
+            :eval              (fn [code]
+                                 (stdout-println "EVALING" code)
+                                 (let [result-p (promise)]
+                                   (swap! eval-requests conj [code result-p])
+                                   (uncatch @result-p)))})
     (transport/send transport
                     (response-for *msg*
                                   {:out "HIJACKING REPL!"}))
@@ -69,7 +79,7 @@
                     (response-for *msg*
                                   {:status #{:done}}))
     (loop []
-      (when-not (realized? p)
+      (when-not (realized? unbreak-p)
         (if-let [[code result-p] (pop-eval-request)]
           (let [code' (format "(fn [{:syms [%s]}]\n%s\n)"
                               (clojure.string/join " " (keys locals))
@@ -79,15 +89,20 @@
                       ((binding [*ns* ns] (eval (read-string code'))) locals))))
           (Thread/sleep 50))
         (recur)))
+    (stdout-println "UNBROKEN")
     nil))
 
 (defn unbreak!
   []
-  (let [{session-id ::session-id} *msg*
-        p (get-in @info [session-id :promise])]
+  (let [{session-id ::orig-session-id} *msg*
+        p (-> @info
+              (get session-id)
+              (peek)
+              (:unbreak))]
     (assert p)
+    ;; TODO: dissoc as well? (minor memory leak)
+    (swap! info update-in [session-id] pop)
     (deliver p nil)
-    (swap! info dissoc session-id)
     nil))
 
 (defmacro break!
@@ -103,29 +118,61 @@
     (recv [this] (transport/recv t))
     (recv [this timeout] (transport/recv t timeout))
     (send [this msg]
-      (.println System/out (str "SENT: " (pr-str msg)))
+      (stdout-println "SENT:" (pr-str msg))
       (transport/send t msg))))
+
+(defn wrap-transport-sub-session
+  [t from-session to-session]
+  (reify transport/Transport
+    (recv [this] (transport/recv t))
+    (recv [this timeout] (transport/recv t timeout))
+    (send [this msg]
+      (let [msg' (cond-> msg (= from-session (:session msg)) (assoc :session to-session))]
+        (transport/send t msg')))))
 
 (defn ^:private wrap-eval
   [{:keys [op code session] :as msg}]
-  (cond-> msg
-          (and (= "eval" op) (contains? @info session))
-          (assoc :code
-            (pr-str
-             `((get-in @info [~session :eval])
-               ~code)))))
+  (let [nested-session-id (-> @info
+                              (get session)
+                              (peek)
+                              (:nested-session-id))]
+    (cond-> msg
+            nested-session-id
+            (-> (assoc :session nested-session-id)
+                (update-in [:transport] wrap-transport-sub-session nested-session-id session))
+
+
+            (and nested-session-id (= "eval" op))
+            (assoc :code
+              (pr-str
+               `((-> @info
+                     (get ~session)
+                     (peek)
+                     (:eval))
+                 ~code))))))
 
 (defn handle-debug
   [handler {:keys [transport op code session] :as msg}]
-  (.println System/out (str "RECEIVED: " (pr-str msg)))
+  (stdout-println "RECEIVED: " (pr-str msg))
   (-> msg
       (update-in [:transport] wrap-transport)
-      (assoc ::session-id session)
-      #_(wrap-eval)
+      (assoc ::orig-session-id session
+             ::nest-session (fn []
+                              {:post [%]}
+                              (let [p (promise)]
+                                (handler {:session session,
+                                          :op "clone",
+                                          :transport (reify transport/Transport
+                                                       (send [_ msg]
+                                                         (deliver p msg)))})
+                                (:new-session @p))))
+      (wrap-eval)
       (handler)))
 
 (defn wrap-debug
   [handler]
+  ;; having handle-debug as a separate function makes it easier to do
+  ;; interactive development on this middleware
   (fn [msg] (handle-debug handler msg)))
 
 
