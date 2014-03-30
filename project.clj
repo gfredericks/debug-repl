deccabcefdcb6180e1aae2ba939d25e4a370c83c
Gary Fredericks <fredericksgary@gmail.com>
2014-03-30 11:26:55 -0500

    Setup lein-release config

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/fredericksgary/debug-repl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :lein-release {:deploy-via :clojars})
