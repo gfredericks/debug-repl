5a8961d000b84b7b77113b21f81618fa73be44b3
Gary Fredericks <fredericksgary@gmail.com>
2014-11-17 17:12:31 -0600

    Version 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/debug-repl "0.0.3"
+(defproject com.gfredericks/debug-repl "0.0.4-SNAPSHOT"
   :description "A Clojure debug repl as nrepl middleware."
   :url "https://github.com/fredericksgary/debug-repl"
   :license {:name "Eclipse Public License"
