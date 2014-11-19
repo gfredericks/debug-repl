80e30f623120e51b5c2328f651f6a705f4e17757
Gary Fredericks <fredericksgary@gmail.com>
2014-11-18 20:21:37 -0600

    Easy test running on 3 clojure versions

--- project.clj
+++ project.clj
@@ -5,5 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :dev {:plugins [[com.gfredericks/nrepl-53-monkeypatch "0.1.0"]]}})
+  :profiles {:1.3 {:dependencies [^:replace [org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [^:replace [org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [^:replace [org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [^:replace [org.clojure/clojure "1.6.0"]]}
+             :dev {:plugins [[com.gfredericks/nrepl-53-monkeypatch "0.1.0"]]}}
+  ;; use `lein all test` to run the tests on all versions
+  ;;
+  ;; Skipping 1.3 because I'm not in the mood to do backpat tricks for
+  ;; ex-info.
+  :aliases {"all" ["with-profile" "+1.4:+1.5:+1.6"]})
